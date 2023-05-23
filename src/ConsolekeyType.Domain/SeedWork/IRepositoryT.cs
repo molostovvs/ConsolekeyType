cc71adc61c7ae84aeb1bcad811dcc1a29a01287f
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-05-23 13:52:26 +0300

    changed the body of the IRepository

--- src/ConsolekeyType.Domain/SeedWork/IRepositoryT.cs
+++ src/ConsolekeyType.Domain/SeedWork/IRepositoryT.cs
@@ -1,9 +1,6 @@
 namespace ConsolekeyType.Domain.SeedWork;
 
-public class IRepository<T> where T : IAggregateRoot
+public interface IRepository<T> where T : IAggregateRoot
 {
-    private string _connectionString { get; }
-
-    public IRepository(string connectionString)
-        => _connectionString = connectionString;
+    private protected string _connectionString { get; }
 }
